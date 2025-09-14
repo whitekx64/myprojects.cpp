5fd569e3589e855e1b9e63af145afba9230f9f43
whitekx64 <plackplack558@gmail.com>
2025-09-14 23:01:07 +0300

    Привет 2

---
+++ var.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age = 15;
+    string name = "Дурачок";
+    
+    cout << "Привет, меня зовут " << name << ". Мне " << age << " Лет" << endl;
+
+    return 0;
+}
